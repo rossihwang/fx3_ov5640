88665bbe6e29c975b100bb6ba13974c7cfb5225c
rossihwang <rossihwang@gmail.com>
2021-08-02 23:05:18 +0800

    Add parameters check for build_image.sh

--- tools/build_image.sh
+++ tools/build_image.sh
@@ -2,9 +2,19 @@
 
 ELF2IMG=${FX3_INSTALL_PATH}/util/elf2img/elf2img
 
-if [[ $1 == "i2c" ]]
+if (($# < 2))
 then
-    ${ELF2IMG} -i $2 -o $3 -i2cconf 0x1c -vectorload yes
+    echo "Usage: $0 <elf_file> <img_file> [img_type](i2c, spi)"
+    exit
+fi
+
+ELF_FILE=$1
+IMG_FILE=$2
+DEV=${3:-spi}
+
+if [[ $DEV == "i2c" ]]
+then
+    ${ELF2IMG} -i ${ELF_FILE} -o ${IMG_FILE} -i2cconf 0x1c -vectorload yes
 else
-    ${ELF2IMG} -i $1 -o $2 -i2cconf 0x00 -vectorload yes
+    ${ELF2IMG} -i ${ELF_FILE} -o ${IMG_FILE} -i2cconf 0x00 -vectorload yes
 fi
